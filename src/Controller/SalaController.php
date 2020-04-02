c7428bd8eb1e71f4d8c8769a2d3d2a60fbb94577
RenanBrs <renan.barbosards@gmail.com>
2020-04-02 19:18:14 -0300

    criação da sala que retorna token e validação

---
+++ src/Controller/SalaController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class SalaController extends AbstractController
+{
+    /**
+     * @Route("/saude/sala", name="sala")
+     */
+    public function index(Request $request)
+    { 
+        $data = json_decode($request->getContent(), true);
+
+        if( $this->validarRequisicao($request) ){
+            return $this->json([ 
+                "result"=>true,
+                "message"=>"Voce será redirecionado para sala virtual de atendimento. Se você não for redirecionado, clique aqui.",
+                "redirect"=>"https://meet.jit.si/ddtqssi",
+                "token válido"=>$data['token'] 
+            ]);
+        }else{
+            return $e =  new Exception("Error Processing Request", 1);
+        }
+  
+        // return $this->render('sala/index.html.twig', [
+        //     'controller_name' => 'SalaController',
+        // ]);
+    }
+ 
+
+    protected function validarRequisicao(Request $request)
+    {
+        if (0 !== strpos($request->headers->get('Content-Type'), 'application/json')) {
+            throw new \InvalidArgumentException("A requisição precisa conter cabeçalho application/json");
+        }
+
+        $data = json_decode($request->getContent(), true);
+  
+        if( !isset($data['token']) ) {
+            throw new \InvalidArgumentException("A requisição não contém todas as informações.");
+        }else{
+            return true;
+        }
+ 
+    }//validarRequisicao
+
+}
